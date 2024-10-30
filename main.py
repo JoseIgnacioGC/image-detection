e80b61450caf3a3d36ddf43b97ef5242d1027f50
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2024-10-30 18:27:02 -0300

    added detection in real time

--- main.py
+++ main.py
@@ -1,22 +1,68 @@
-from src.img_captioning.generate_description import generate_image_description
-from src.img_captioning.charge_model import charge_model
-from src.get_images import raw_images
-from src.shell_question import get_processor_option
+import cv2
+from cv2.gapi.wip.draw import Image
 
-# https://huggingface.co/meta-llama/Llama-3.2-11B-Vision-Instruct
+from src.shell_question import get_processor_option, ProcessorOption
 
-processor_option = get_processor_option()
-processor_and_model = charge_model(processor_option)
-print("Model ready\n")
+option = get_processor_option()
 
-for raw_image in raw_images:
+if option == ProcessorOption.GPU:
+    from src.img_captioning.models.by_gpu import (
+        generate_image_description,
+        get_processor_and_model,
+    )
+else:
+    from src.img_captioning.models.by_cpu import (
+        generate_image_description,
+        get_processor_and_model,
+    )
+
+processor, model = get_processor_and_model()
+
+def convert_opencv_to_pil(cv_image):
+    return Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
+
+def capture_image_from_camera():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("no hay camara")
+            break
+
+        cv2.imshow('espacio pa capturar la foto', frame)
+
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord(' '):
+            pil_image = convert_opencv_to_pil(frame)
+            break
+        elif key == ord('q'):
+            pil_image = None
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return pil_image
+
+raw_image = capture_image_from_camera()
+
+if raw_image:
     try:
         description = generate_image_description(
-            processor_option=processor_option,
-            raw_image=raw_image,
-            **processor_and_model
+            processor=processor, model=model, raw_image=raw_image
         )
+        print("descripcion:", description)
     except Exception as e:
-        print(e)
-    else:
-        print(description)
+        print(f"Error: {e}")
+else:
+    print("no se capturo imagen.")
+
+
+# TODO: give the option to change processor
+# from src.shell_question import ProcessorOption, get_processor_option
+# processor = get_processor_option()
+# if processor == ProcessorOption.GPU:
+# import src.img_captioning.by_gpu
+# else:
+# import src.img_captioning.by_CPU
