64b4a4cadcc27ec1b14f0640e8a8653bff2cf6a0
Jose Leiva <email.joseleiva@gmail.com>
2024-10-31 15:30:18 -0300

    Merge & solve conflicts

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
+from src.img_captioning.utils import ImageDescriptionParams
+from src.capture_image import convert_opencv_to_pil
+from src.img_captioning.model_controller import charge_model
+from src.img_captioning.img_description_controller import generate_image_description
+from src.shell_question import get_processor_option
+
 import cv2
-from PIL import Image
-from src.shell_question import get_processor_option, ProcessorOption
 
-letterColors = (0, 255, 0)
-option = get_processor_option()
 
-if option == ProcessorOption.GPU:
-    from src.img_captioning.models.by_gpu import (
-        generate_image_description,
-        get_processor_and_model,
-    )
-else:
-    from src.img_captioning.models.by_cpu import (
-        generate_image_description,
-        get_processor_and_model,
-    )
+processor_option = get_processor_option()
+processor_and_model = charge_model(processor_option)
+print("Model ready\n")
 
-processor, model = get_processor_and_model()
-
-def convert_opencv_to_pil(cv_image):
-    return Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
 
 def capture_image_from_camera():
     cap = cv2.VideoCapture(0)
+    letterColors = (0, 255, 0)
     description = "espacio pa capturar y q pa salir"
     text_band_height = 60
     font_scale = 0.6
@@ -37,49 +29,51 @@ def capture_image_from_camera():
         display_frame = cv2.copyMakeBorder(
             frame, 0, text_band_height, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0)
         )
-        lines = description.split('\n')
+        lines = description.split("\n")
         y_offset = frame.shape[0] + 20
 
         for line in lines:
-            cv2.putText(display_frame, line, (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX,
-                        font_scale, letterColors, font_thickness, lineType=cv2.LINE_AA)
+            cv2.putText(
+                display_frame,
+                line,
+                (10, y_offset),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                font_scale,
+                letterColors,
+                font_thickness,
+                lineType=cv2.LINE_AA,
+            )
             y_offset += 20
-        cv2.imshow('camara', display_frame)
+        cv2.imshow("camara", display_frame)
 
         key = cv2.waitKey(1) & 0xFF
-        if key == ord(' '):
+        if key == ord(" "):
             pil_image = convert_opencv_to_pil(frame)
+            params = ImageDescriptionParams(pil_image, processor_and_model)
             description = generate_image_description(
-                processor=processor, model=model, raw_image=pil_image
+                processor_option=processor_option, params=params
             )
-            description = '\n'.join([description[i:i+60] for i in range(0, len(description), 60)])
-        elif key == ord('q'):
+            description = "\n".join(
+                [description[i : i + 60] for i in range(0, len(description), 60)]
+            )
+        elif key == ord("q"):
             break
+
+        keywords_theft = [
+            "punch",
+            "stabbed",
+            "danger",
+            "scared",
+            "pistol",
+            "wepon",
+            "gun",
+        ]  # no se q mas agregar
+        is_a_thief = any(keyword in description for keyword in keywords_theft)
+        if is_a_thief:
+            letterColors = (255, 0, 0)
 
     cap.release()
     cv2.destroyAllWindows()
 
-raw_image = capture_image_from_camera()
 
-if raw_image:
-    try:
-        description = generate_image_description(
-            processor=processor, model=model, raw_image=raw_image
-        )
-
-        if (["punch", "stabbed", "danger", "scared", "pistol", "weapon"] in description): # no se q mas agregar
-            letterColors = (255, 0, 0)
-
-    except Exception as e:
-        print(f"Error: {e}")
-else:
-    print("no se capturo imagen.")
-
-
-# TODO: give the option to change processor
-# from src.shell_question import ProcessorOption, get_processor_option
-# processor = get_processor_option()
-# if processor == ProcessorOption.GPU:
-# import src.img_captioning.by_gpu
-# else:
-# import src.img_captioning.by_CPU
+capture_image_from_camera()
--- src/capture_image.py
+++ src/capture_image.py
@@ -3,29 +3,29 @@ from cv2.typing import MatLike
 from PIL import Image
 
 
-def __convert_opencv_to_pil(cv_image: MatLike) -> Image.Image:
+def convert_opencv_to_pil(cv_image: MatLike) -> Image.Image:
     return Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
 
 
-def capture_image_from_camera() -> Image.Image | None:
-    cap = cv2.VideoCapture(0)
-    pil_image = None
+# def capture_image_from_camera() -> Image.Image | None:
+#     cap = cv2.VideoCapture(0)
+#     pil_image = None
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("no hay camara")
-            break
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             print("no hay camara")
+#             break
 
-        cv2.imshow("espacio pa capturar la foto", frame)
+#         cv2.imshow("espacio pa capturar la foto", frame)
 
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord(" "):
-            pil_image = __convert_opencv_to_pil(frame)
-            break
-        elif key == ord("q"):
-            break
+#         key = cv2.waitKey(1) & 0xFF
+#         if key == ord(" "):
+#             pil_image = __convert_opencv_to_pil(frame)
+#             break
+#         elif key == ord("q"):
+#             break
 
-    cap.release()
-    cv2.destroyAllWindows()
-    return pil_image
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     return pil_image
