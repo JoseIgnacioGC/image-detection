a9ab8f4612b2e7073e26e37f3a8f40e1aca72a75
Jose Leiva <email.joseleiva@gmail.com>
2024-11-10 15:36:49 -0300

    Refactor emails

--- src/emails.py
+++ src/emails.py
@@ -1,14 +1,17 @@
+from src.get_env_var import credentials
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-import window 
+import window
 
-def email():
+
+def send_email():
     smtp_server = "smtp.gmail.com"
     smtp_port = 465
-    username = ""
-    password = ""
+    username = credentials.email_server_email
+    password = credentials.email_server_password
 
     # Crear el mensaje
     msg = MIMEMultipart()
@@ -16,14 +19,13 @@ def email():
     msg["To"] = " "
     msg["Subject"] = "Prueba De Correo"
 
-
     Cuerpo_Correo = "Hola buenas tardes"
     msg.attach(MIMEText(Cuerpo_Correo, "plain"))
 
-    # abre la imagen 
+    # abre la imagen
     imagen_ruta = open("imagenes/pug.png", "rb")
 
-    imagen= MIMEImage(imagen_ruta.read())
+    imagen = MIMEImage(imagen_ruta.read())
     imagen.add_header("Content-Disposition", "attachment", filename="pug.png")
     msg.attach(imagen)
     imagen_ruta.close()
@@ -36,4 +38,6 @@ def email():
             print("Correo enviado exitosamente!")
     except Exception as e:
         print(f"Error al enviar correo: {e}")
-window.open_Window(email)
+
+
+window.open_window(send_email)
--- src/window.py
+++ src/window.py
@@ -1,22 +1,21 @@
 import tkinter as tk
 
 
-# Titulo y dimensiones de la ventana 
-def open_Window(enviar_correo):
+# Titulo y dimensiones de la ventana
+def open_window(send_email):
     ventana = tk.Tk()
     ventana.title("Enviar Correo")
     ventana.geometry("200x100")
-    # aca es el texto que va a mostrar 
-    texto="¿Quieres enviar el correo?"
+    # aca es el texto que va a mostrar
+    texto = "¿Quieres enviar el correo?"
     etiqueta = tk.Label(ventana, text=texto)
 
     etiqueta.pack(padx=10, pady=20)
-        
-    boton_enviar = tk.Button(ventana, text="Enviar", command=enviar_correo)
+
+    boton_enviar = tk.Button(ventana, text="Enviar", command=send_email)
     boton_enviar.pack(side=tk.LEFT, padx=20)
-    
-        
+
     boton_cancelar = tk.Button(ventana, text="Cancelar", command=ventana.destroy)
     boton_cancelar.pack(side=tk.RIGHT, padx=20)
-        
+
     ventana.mainloop()
